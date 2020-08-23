eed0dacf26dd6d5ac1b14d7931c1f12b629e33e6
Romi <rockwithromi@gmail.com>
2020-08-23 17:17:19 -0400

    Initial commit

---
+++ Book Queries/BookQueries.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Syed_Syed_Exercise01
+{
+    partial class BookQueries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.queriesComboBox = new System.Windows.Forms.ComboBox();
+            this.selectQueryLabel = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // queriesComboBox
+            // 
+            this.queriesComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.queriesComboBox.FormattingEnabled = true;
+            this.queriesComboBox.Items.AddRange(new object[] {
+            "Number of authors grouped by title, sorted by title",
+            "All titles grouped by author name, sorted by author"});
+            this.queriesComboBox.Location = new System.Drawing.Point(12, 36);
+            this.queriesComboBox.Name = "queriesComboBox";
+            this.queriesComboBox.Size = new System.Drawing.Size(748, 28);
+            this.queriesComboBox.TabIndex = 1;
+            this.queriesComboBox.SelectedIndexChanged += new System.EventHandler(this.queriesComboBox_SelectedIndexChanged);
+            // 
+            // selectQueryLabel
+            // 
+            this.selectQueryLabel.AutoSize = true;
+            this.selectQueryLabel.Location = new System.Drawing.Point(8, 9);
+            this.selectQueryLabel.Name = "selectQueryLabel";
+            this.selectQueryLabel.Size = new System.Drawing.Size(110, 20);
+            this.selectQueryLabel.TabIndex = 2;
+            this.selectQueryLabel.Text = "Select a query";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(12, 95);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(748, 334);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 72);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(55, 20);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Result";
+            // 
+            // BookQueries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 441);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.selectQueryLabel);
+            this.Controls.Add(this.queriesComboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BookQueries";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Queries";
+            this.Load += new System.EventHandler(this.BookQueries_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox queriesComboBox;
+        private System.Windows.Forms.Label selectQueryLabel;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Book Queries/BookQueries.cs
@@ -0,0 +1,112 @@
+﻿using BooksLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Syed_Syed_Exercise01
+{
+    public partial class BookQueries : Form
+    {
+        public BookQueries()
+        {
+            InitializeComponent();
+        }
+
+        BooksEntities dbContext = new BooksEntities();
+
+        /// <summary>
+        /// This method handle the BookQueries_Load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BookQueries_Load(object sender, EventArgs e)
+        {
+            // set the ComboBox to show the default query
+            queriesComboBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// This method handle the queriesComboBox_SelectedIndexChanged event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void queriesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // set the data displayed according to what is selected
+                switch (queriesComboBox.SelectedIndex)
+                {
+                    //a.Get a count of all the authors grouped by title, sorted by title. 
+                    //  It should display a title and number of authors who have written that title.
+                    case 0:
+                        var authorsByTitle =
+                            from book in dbContext.Titles
+                            orderby book.Title1
+                            select new { Title = book.Title1, Count = book.Authors.Count() };
+
+                        outputTextBox.Clear();
+
+                        outputTextBox.AppendText($"\n-------------------------------------------------------------------------------");
+                        outputTextBox.AppendText($"\r{"Title",-60} {"Number of Authors"}");
+                        outputTextBox.AppendText($"\n-------------------------------------------------------------------------------");
+                        foreach (var book in authorsByTitle)
+                        {
+                            outputTextBox.AppendText($"\r\n{book.Title,-60} {book.Count}");
+                            outputTextBox.AppendText($"\n-------------------------------------------------------------------------------");
+                        }
+
+                        break;
+
+                    //b.Get a list of all the titles grouped by author name, sorted by author; 
+                    //  for a given author name sort the titles alphabetically.
+                    case 1:
+                        var titlesByAuthor =
+                            from author in dbContext.Authors
+                            orderby author.LastName, author.FirstName
+                            select new
+                            {
+                                Name = author.FirstName + " " + author.LastName,
+                                Titles =
+                                  from book in author.Titles
+                                  orderby book.Title1
+                                  select book.Title1
+                            };
+
+                        outputTextBox.Clear();
+
+                        outputTextBox.AppendText($"\n-------------------------------------------------------------------------------");
+                        outputTextBox.AppendText("\rTitles grouped by author name:");
+                        outputTextBox.AppendText($"\n-------------------------------------------------------------------------------");
+
+                        // display titles written by each author, grouped by author
+                        foreach (var author in titlesByAuthor)
+                        {
+                            // display author's name
+                            outputTextBox.AppendText($"\r\n{author.Name}:");
+
+                            // display titles written by that author
+                            foreach (var title in author.Titles)
+                            {
+                                outputTextBox.AppendText($"\r\n\t{title}");
+                            }
+                        }
+
+                        break;
+                } // end switch
+            } // end try
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message, "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        } // end queriesComboBox_SelectedIndexChanged
+    } // end class
+} // end namespace
